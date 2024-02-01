e3a2913233a89f3299b1829c010df25d1ec89b46
jacomar <jacomar304@hotmail.com>
2024-02-01 19:51:04 +0100

    Creado el módulo 'utilities', que alberga las librerías 'ConsoleColors.kt' y 'Utilities.kt', y el módulo 'models', que alojará los ficheros de las clases del proyecto.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 rootProject.name = "Lampara"
-
+include("src:utilities")
+findProject(":src:utilities")?.name = "utilities"
+include("src:models")
+findProject(":src:models")?.name = "models"
